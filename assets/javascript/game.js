207e1f3d6206bce7e87ca73e8ad105576ae5396f
Charles Prestia <Charles Prestia>
2016-08-12 17:30:50 -0400

    basic outline of html/css/js with images

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+	
+	var userChar = "";
+	var userHP = 0;
+	var defenderChar = "";
+	var defenderHP = 0;
+
+	var characterArray = [
+		{
+			'charName': '',
+			'charImg': 'assets/images/',
+			'startingHP': '',
+			'initialAtkVal': '',
+			'counterAtkVal': '',
+		},
+		{
+			'charName': '',
+			'charImg': 'assets/images/',
+			'startingHP': '',
+			'initialAtkVal': '',
+			'counterAtkVal': '',
+		},
+		{
+			'charName': '',
+			'charImg': 'assets/images/',
+			'startingHP': '',
+			'initialAtkVal': '',
+			'counterAtkVal': '',
+		},
+		{
+			'charName': '',
+			'charImg': 'assets/images/',
+			'startingHP': '',
+			'initialAtkVal': '',
+			'counterAtkVal': '',
+		}
+	];
+
+
+
+	function   (){
+
+	}
+}
